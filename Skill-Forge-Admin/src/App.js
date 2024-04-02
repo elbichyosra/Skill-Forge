1a0fe58a2e307151049d9173a14b855bfcc247a8
elbich yosra <yosra.elbich@esprit.tn>
2024-04-02 21:30:06 +0100

    docker compose

---
+++ Skill-Forge-Admin/src/App.js
@@ -0,0 +1,74 @@
+import { lazy, Suspense, useEffect } from 'react';
+
+/// Components
+import Index from "./jsx";
+import { connect, useDispatch } from 'react-redux';
+import { Switch, withRouter,Route } from 'react-router-dom';
+
+/// Style
+import "./vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
+import "./css/style.css";
+
+import useAuth from './hooks/useAuth';
+
+// import Error403 from './jsx/pages/Error403';
+const Error403 = lazy(() => import("./jsx/pages/Error403"));
+
+
+function App (props) {
+    const [isLogin, token, handleLogout, userName] = useAuth();
+  console.log(token);
+  console.log(userName)
+    let routes = (  
+        <Switch>
+        <Route path='/page-error-403' component={Error403} />
+        </Switch> 
+    );
+   
+		return (
+			<>
+                {isLogin?(<Suspense fallback={
+                    <div id="preloader">
+                        <div className="sk-three-bounce">
+                            <div className="sk-child sk-bounce1"></div>
+                            <div className="sk-child sk-bounce2"></div>
+                            <div className="sk-child sk-bounce3"></div>
+                        </div>
+                    </div>  
+                   }
+                >
+                       
+                    <Index />
+                    
+                
+                   
+                </Suspense>
+                ):( 
+                <Suspense fallback={
+                    <div id="preloader">
+                        <div className="sk-three-bounce">
+                            <div className="sk-child sk-bounce1"></div>
+                            <div className="sk-child sk-bounce2"></div>
+                            <div className="sk-child sk-bounce3"></div>
+                        </div>
+                    </div>  
+                   }
+                > 
+                 
+                  <Error403 handleLogout={handleLogout} />
+                
+              
+            </Suspense>
+           
+              )}
+               
+            </>
+        );
+	
+
+};
+
+
+
+export default withRouter(App); 
+
